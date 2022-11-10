33d5bff8d0afa04d09f9b6117868a8c9c6029efd
hugofknjansson <116803291+hugofknjansson@users.noreply.github.com>
2022-11-10 12:13:14 +0100

    skola2

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+let images = ["img/wecrashed.jpg","img/wecrashed1.jpg.webp","img/wecrashed2.jpg","img/wecrashed3.jpg.webp"]
+
+function changeImage(){
+    document.getElementById("background").src = images[2];
+}
